3fdbb85f9abdbf0a3a3aec14fe209dae306e8208
richmond089 <wrichmond359@gmail.com>
2024-10-29 04:24:49 +0800

    about
    add changes

---
+++ about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link href="./output.css" rel="stylesheet"> -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/05be2a39a6.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Lex Eat Na</title>
+</head>
+
+<body>
+    <!-- NavBar -->
+    <?php include "./partials/navbar.php"; ?>
+
+
+    <!-- <div class="h-screen">
+        <div class="text-center font-semibold text-4xl">
+            About Us
+        </div>
+        <div class="  bg-slate-500 h-3/4 w-2/4 flex items-center justify-center ">
+            Welcome to LEX EAT NAA, a fusion of culinary flavors, a touch of today’s technology and a seamless dining
+            experience. A humble food cart founded in 2018, and then steadily growing, our customer base and
+            operational needs have also grown. To meet demand and offer better service,
+            we’ve harnessed our Online Ordering and Point of Sale (POS) system with email notification,
+            e-receipts to deliver a convenient transaction. We try to simplify order processing, minimize errors and
+            real time financial tracking, so that customers are better served and business is better run.
+            This bottom to top dribble txt is how we’re introducing LEX EAT NAA - Innovation meets flavor and
+            serves you better, faster, smarter…
+
+            <div>
+                <img src="./images/abt.jpg" alt="" class="w-[2000px] h-auto rounded-md pr-9">
+            </div>
+        </div>
+
+    </div> -->
+    <div class="text-center font-semibold text-4xl mt-10">
+        About Us
+    </div>
+
+    <div class="card m-20 shadow-2xl rounded-2xl">
+        <div class="container items-center w-2/3 text-justify p-10">
+            <img src="./images/abt.jpg" alt="Avatar" class="h-auto w-1/2 rounded mx-auto mb-10">
+
+            <p>Welcome to LEX EAT NAA, a fusion of culinary flavors, a touch of today’s technology and a seamless dining experience. A humble food cart founded in 2018, and then steadily growing, our customer base and operational needs have also grown. To meet demand and offer better service, we’ve harnessed our Online Ordering and Point of Sale (POS) system with email notification, e-receipts to deliver a convenient transaction. We try to simplify order processing, minimize errors and real time financial tracking, so that customers are better served and business is better run. This bottom to top dribble txt is how we’re introducing LEX EAT NAA - Innovation meets flavor and serves you better, faster, smarter…</p>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <?php include './partials/footer.php'; ?>
+
+</body>
+
+</html>
