689b65b06650225dcf9c5637dbe6bf1b938ac9fa
richmond089 <wrichmond359@gmail.com>
2024-10-29 05:51:16 +0800

    dashboard
    add changes

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require '../database/db.php';
 
 // Check if user is logged in
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
@@ -24,6 +25,60 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <div class="ml-56 bg-white p-6 rounded-lg shadow-md w-full text-center">
         <h2 class="text-2xl font-bold mb-4">Welcome, <?php echo $_SESSION['username']; ?>!</h2>
         <p>You are logged in.</p>
+
+        <div class="flex justify-evenly m-40">
+            <div class="dashboard-cards my-10 bg-slate-300 w-52 h-1/3">
+                <?php
+                $sql = "SELECT * FROM addproducts";
+                $res = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($res);
+                ?>
+                <h2 class="font-bold text-3xl "><?php echo $count; ?></h2>
+                <p class="p-2 text-lg">Products</p>
+            </div>
+            <div class="dashboard-cards my-10 bg-slate-300 w-52 h-1/3">
+                <?php
+                $sql = "SELECT * FROM addproducts";
+                $res = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($res);
+                ?>
+                <h2 class="font-bold text-3xl "><?php echo $count; ?></h2>
+                <p class="p-2 text-lg">Products</p>
+            </div>
+            <div class="dashboard-cards my-10 bg-slate-300 w-52 h-1/3">
+                <?php
+                $sql = "SELECT * FROM addproducts";
+                $res = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($res);
+                ?>
+                <h2 class="font-bold text-3xl "><?php echo $count; ?></h2>
+                <p class="p-2 text-lg">Products</p>
+            </div>
+            <div class="dashboard-cards my-10 bg-slate-300 w-52 h-1/3">
+                <?php
+                $sql = "SELECT * FROM addproducts";
+                $res = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($res);
+                ?>
+                <h2 class="font-bold text-3xl "><?php echo $count; ?></h2>
+                <p class="p-2 text-lg">Products</p>
+            </div>
+
+        </div>
+        <div class="flex-col">
+            <div class="bg-slate-300">
+                <?php
+                $sql = "SELECT * FROM addproducts";
+                $res = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($res);
+                ?>
+                <h2 class="font-bold text-3xl"><?php echo $count; ?></h2>
+                <p class="p-2 text-lg">Total Revenue</p>
+            </div>
+
+        </div>
+
+
     </div>
 </body>
 
